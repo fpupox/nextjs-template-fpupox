75555d5fa587002abd6a2d04e05a8a08a5f1d6ee
fpupox <64444703+fpupox@users.noreply.github.com>
2024-03-04 05:03:20 +0000

    Template Ready

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import { Button, Col, Row } from "react-bootstrap";
+
+export function Jumbotron () {
+    return <Row>
+            <Col>
+            <h1>Here</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis beatae velit itaque sunt pariatur, alias ratione quas esse? Maxime ullam laudantium nulla modi, ea quis aperiam voluptatem vero excepturi inventore!</p>
+            <Button variant='primary'>CTA</Button>       
+            </Col>
+           </Row>
+}
